75ee6750787796951c755443d7fd6013cb198f7c
Jake Lee <leejakejh@gmail.com>
2023-01-18 13:26:45 -0800

    server setup

---
+++ src/column.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Column extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+
+  render() {
+
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+
+export default Column;
---
+++ src/game.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './game.scss';
+
+function Game(props) {
+  // const [board, setBoard] = useState();
+
+
+  return (
+    <div id='board'></div>
+  )
+}
+
+
+export default Game;
